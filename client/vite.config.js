c6237c0fe7081cbf8592a08a672f9c96223664a5
EkeneFidel <echukwurah99@gmail.com>
2023-05-07 20:20:08 +0100

    feat: completed client side

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import http from "https";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), "");
+export default ({ mode }) => {
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
 
-    return {
+    return defineConfig({
         plugins: [react()],
-        server: {
-            proxy: {
-                "/api": {
-                    target: env.VITE_SERVERURL,
-                    changeOrigin: true,
-                    secure: false,
-                    agent: new http.Agent(),
-                },
-            },
-        },
-    };
-});
+    });
+};
